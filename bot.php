d92f24c0457413a2e77f92b1848f82d31814dd0f
kumo2kumo <amgokuraku@gmail.com>
2019-07-26 14:49:38 +0900

    add phpdot

--- bot.php
+++ bot.php
@@ -4,8 +4,22 @@ require_once('twitteroauth-master/autoload.php');
 require_once('twitteroauth-master/src/TwitterOAuth.php');
 use Abraham\TwitterOAuth\TwitterOAuth;
 
-require_once('config.php');
+// require_once('config.php');
+require '../../php/vendor/autoload.php';
+$dotenv  = Dotenv\Dotenv :: create(__DIR__); 
+$dotenv->load();
+
 //db connect
+function db_conn(){
+  $dbn ='mysql:dbname=gsf_d03_db22;charset=utf8;port=3306;host=localhost';
+  $user = 'root';
+  $pwd = '';
+  try {
+   return new PDO($dbn, $user, $pwd);
+  } catch (PDOException $e) {
+    exit('dbError:'.$e->getMessage());
+  }
+}
 $pdo = db_conn();
 
 //一緒のテーブル作成
@@ -16,6 +30,14 @@ $sql = "SELECT * FROM (SELECT word FROM kanki UNION SELECT word FROM ohki) as GO
 $stmt = $pdo->prepare($sql);
 $status = $stmt->execute();
 
+//key get
+$consumer_key  =  getenv('consumer_key');
+$consumer_sec  =  getenv('consumer_sec');
+$access_token  =  getenv('access_token');
+$access_sec  =  getenv('access_sec');
+
+
+
 try{
   if ($status==false) {
     $error = $stmt->errorInfo();
@@ -24,7 +46,7 @@ try{
     if($result = $stmt->fetch(PDO::FETCH_ASSOC)){
       $twitter = $result["word"];
     // echo($result['word']);
-      $connection = new TwitterOAuth(consumer_key,consumer_sec,access_token,access_sec);
+      $connection = new TwitterOAuth($consumer_key,$consumer_sec,$access_token,$access_sec);
       $statues = $connection->post("statuses/update", ["status" => $twitter]);
 
     }else{
